c028d1f7b9141e1a9a67c94c4c699086a0093065
nitin343 <61547659+nitin343@users.noreply.github.com>
2023-03-15 11:30:19 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
       roboto_Condensed: ["var(--font-roboto_Condensed), ...fontfamily.sans-serif"],
     },
     screens: {
-      'md':  {'min': '280px', 'max': '768px'},
+      'md':  {'min': '280px', 'max': '1024px'},
       'laptop': '1024px',  
       'desktop': '1280px',
     },
